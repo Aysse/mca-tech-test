2b745b2453ce3808a5c3fc8aeba315f51dd371a4
Aysse <aysse4@gmail.com>
2023-03-09 22:39:15 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { HiOutlineDeviceMobile } from 'react-icons/hi'
+function App () {
+  return (
+    <div className='page'>
+      <h1>Mobile store <HiOutlineDeviceMobile /></h1>
+    </div>
+  )
+}
+
+export default App
