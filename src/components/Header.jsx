c45e004d622ea3eb93f2dcdffafc55eebf387ee8
Aysse <aysse4@gmail.com>
2023-03-19 23:09:43 +0000

    Add header indicator

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
-import { Link } from 'react-router-dom'
 import './Header.css'
+import { Link, useLocation } from 'react-router-dom'
 
 export function Header () {
+  const location = useLocation()
+
   return (
     <div>
-      <Link to='/' className='linkHome'>Podcaster</Link>
+      <div className='header-columns'>
+        <Link to='/' className='linkHome'>Podcaster</Link>
+        {location.pathname === '/' && <div className='indicator' />}
+      </div>
       <hr style={{ width: '100%', marginTop: '20px' }} />
     </div>
   )
